b00f38d1ef69a5b68736ecf2590362e172eb0fa3
Toomas Mölder <toomas.molder@ria.ee>
2021-01-17 18:47:01 +0200

    Script version and date check improvements, added test and debug options etc

--- bin/make_my.sh
+++ bin/make_my.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ###################################################################
 # Script Name   : make_my.sh
-# Script version: 1.1
-# Script date   : 2020-10-16
+# Script version : 1.2
+# Script date   : 2021-01-17
 # Description   : Make my environment handy
 # Args          : <none>
 # Author        : Toomas Mölder
@@ -13,105 +13,128 @@
 ## Usage: make_my.sh [options]
 ##
 ## Options:
-##   -h, --help    Display this message.
-##   -v, --version Display version.
+##   -h, --help    Display this message
+##   -d, --debug   Display debug messages
+##   -t, --test    Update only .test
+##   -v, --version Display version
 ##   -x, --xtrace  Print a trace of commands and their arguments
-##                 before they are executed.
+##                 before they are executed
 ##
 
 function usage() {
   [ "$*" ] && echo "$0: $*"
-  sed -n '/^## /,/^$/s/^## \{0,1\}//p' "$0"
+  /bin/sed --quiet '/^## /,/^$/s/^## \{0,1\}//p' "$0"
   exit $?
 } 2>/dev/null
 
 function version() {
   [ "$*" ] && echo "$0: Version "
-  printf "$0: version "; grep "^# Script version: " ${0} | awk -F: '{print $2}' | sed -e 's/^[[:space:]]*//'
+  SCRIPT_VERSION=$(/bin/grep --no-messages "^# Script version *: " ${0} | /usr/bin/tail --lines 1 | /usr/bin/awk --field-separator=':' '{print $2}' | /usr/bin/awk '{print $1}' | /bin/sed --expression='s/^[[:space:]]*//' | /usr/bin/bc --mathlib)
+  SCRIPT_DATE=$(/bin/grep --no-messages "^# Script date *: " ${0} | /usr/bin/tail --lines 1 | /usr/bin/awk --field-separator=':' '{print $2}' | /usr/bin/awk '{print $1}' | /bin/sed --expression='s/^[[:space:]]*//')
+  echo "${0} version: ${SCRIPT_VERSION} (${SCRIPT_DATE})"
   exit $?
+} 2>/dev/null
+
+function debug() {
+  if [ "${DEBUG}" == "y" ]; then echo "$@"; fi
 } 2>/dev/null
 
 function update() {
   #
-  # echo "Check parameters and files available"
+  debug "Check parameters and files available"
   #
-  if [ $# -ne 2 ]; then echo "${0}: Warning: function ${FUNCNAME}() MUST have EXACTLY two parameters. Do nothing."; return; fi
+  if [ $# -ne 2 ]; then echo "${0}: Warning: function ${FUNCNAME}() MUST have EXACTLY two parameters, FROM and TO. Do nothing."; return; fi
+
   from="${1}"; to="${2}"
+
   if [ ! -s "${from}" ]; then echo "${0}: Warning: file ${from} does not exist or is empty. Do nothing."; return; fi
   #
-  # echo "Get version and date"
+  debug "Get version and date"
   #
-  FROM_VERSION=$(egrep --no-messages "^[#\"] Script version *: " ${from} | tail --lines 1 | awk -F: '{print $2}' | awk '{print $1}' | sed -e 's/^[[:space:]]*//' | bc --mathlib)
-  FROM_DATE=$(grep --no-messages "^[#\"] Script date *: " ${from} | awk -F: '{print $2}' | awk '{print $1}' | sed -e 's/^[[:space:]]*//')
-  # echo "${from}: version: ${FROM_VERSION} (${FROM_DATE})"
-  TO_VERSION=$(egrep --no-messages "^[#\"] Script version *: " ${to} | tail --lines 1 | awk -F: '{print $2}' | awk '{print $1}' | sed -e 's/^[[:space:]]*//' | bc --mathlib)
-  TO_DATE=$(grep --no-messages "^[#\"] Script date *: " ${to} | awk -F: '{print $2}' | awk '{print $1}' | sed -e 's/^[[:space:]]*//')
-  # echo "${to}: version: ${TO_VERSION} (${TO_DATE})"
+  FROM_VERSION=$(/bin/egrep --no-messages "^[#\"] Script version *: " ${from} | /usr/bin/tail --lines 1 | /usr/bin/awk --field-separator=':' '{print $2}' | /usr/bin/awk '{print $1}' | /bin/sed --expression='s/^[[:space:]]*//' | /usr/bin/bc --mathlib)
+  FROM_DATE=$(/bin/grep --no-messages "^[#\"] Script date *: " ${from} | /usr/bin/tail --lines 1 | /usr/bin/awk --field-separator=':' '{print $2}' | /usr/bin/awk '{print $1}' | /bin/sed --expression='s/^[[:space:]]*//')
+  debug "${from}: version: ${FROM_VERSION} (${FROM_DATE})"
+  TO_VERSION=$(/bin/egrep --no-messages "^[#\"] Script version *: " ${to} | /usr/bin/tail --lines 1 | /usr/bin/awk --field-separator=':' '{print $2}' | /usr/bin/awk '{print $1}' | /bin/sed --expression='s/^[[:space:]]*//' | /usr/bin/bc --mathlib)
+  TO_DATE=$(/bin/grep --no-messages "^[#\"] Script date *: " ${to} | /usr/bin/tail --lines 1 | /usr/bin/awk --field-separator=':' '{print $2}' | /usr/bin/awk '{print $1}' | /bin/sed --expression='s/^[[:space:]]*//')
+  debug "${to}: version: ${TO_VERSION} (${TO_DATE})"
   #
-  # echo "Remove all new line, carriage return, tab characters"
-  # echo "from the string, to allow integer / float comparison."
+  debug "Remove all new line, carriage return, tab characters \
+    from the string, to allow integer / float comparison."
   FROM_VERSION="${FROM_VERSION//[$'\t\r\n ']}"
   FROM_DATE="${FROM_DATE//[$'\t\r\n ']}"
   TO_VERSION="${TO_VERSION//[$'\t\r\n ']}"
   TO_DATE="${TO_DATE//[$'\t\r\n ']}"
-  # echo "${from}: version: ${FROM_VERSION} (${FROM_DATE})"
-  # echo "${to}: version: ${TO_VERSION} (${TO_DATE})"
+  debug "${from}: version: ${FROM_VERSION} (${FROM_DATE})"
+  debug "${to}: version: ${TO_VERSION} (${TO_DATE})"
 
   #
-  # echo "Check existence. If not, then give default value."
-  if [ -z "${FROM_VERSION}" ]; then FROM_VERSION=0.0; fi
+  debug "Check existence. If not, then give default value."
+  if [ -z "${FROM_VERSION}" ]; then FROM_VERSION=0; fi
   if [ -z "${FROM_DATE}" ]; then FROM_DATE=1970-01-01; fi
-  if [ -z "${TO_VERSION}" ]; then TO_VERSION=0.0; fi
+  if [ -z "${TO_VERSION}" ]; then TO_VERSION=0; fi
   if [ -z "${TO_DATE}" ]; then TO_DATE=1970-01-01; fi
-  # echo "${from}: version: ${FROM_VERSION} (${FROM_DATE})"
-  # echo "${to}: version: ${TO_VERSION} (${TO_DATE})"
+  debug "${from}: version: ${FROM_VERSION} (${FROM_DATE})"
+  debug "${to}: version: ${TO_VERSION} (${TO_DATE})"
 
   #
-  # echo "Make update happen only when newest version is not yet present."
-  # TODO: remove previous version from ${to} if exists, to avoid duplicate handys ...
-  # smth like sed -n '/^###################################################################/,$ p' ${to} -i.bak
+  debug "Make update happen only when newest version is not yet present."
   #
-  printf "${to}"
-  if (( $(echo "${TO_VERSION} < ${FROM_VERSION}" | bc --mathlib) || $(echo "${TO_DATE} < ${FROM_DATE}" | bc --mathlib))); then
-    if [ -f ${to} ]; then /bin/cp --preserve ${to} ${to}.bak; fi
-    # Add newline to the end of ${to} if not present yet to avoid script errors
-    [ -n "$(tail --bytes=1 ${to})" ] && printf '\n' >> ${to}
+  printf "==> ${to}"
+  if [ ! -s "${to}" ]; then
+    /bin/cp --preserve ${from} ${to};
+    echo " added to version ${FROM_VERSION} (${FROM_DATE}).";
+    return;
+  fi
+  if (( $(echo "${FROM_VERSION} <= ${TO_VERSION}" | /usr/bin/bc --mathlib) && $(echo "$(/bin/date --date="${FROM_DATE} UTC" +%s) <= $(/bin/date --date="${TO_DATE} UTC" +%s)" | /usr/bin/bc --mathlib) )); then
+    echo " version ${TO_VERSION} (${TO_DATE}) already exists. Did not update."
+  else
+    if [ -s "${to}" ]; then
+      debug "Make backup of ${to} into ${to}.bak"
+      /bin/cp --preserve ${to} ${to}.bak;
+      debug "Remove previous version from ${to} if exists, to avoid duplicate handys ..."
+      /bin/sed --in-place --quiet '/^###################################################################$/,$d' ${to}
+      debug "Add newline to the end of ${to} if not present yet to avoid script errors."
+      [ -n "$(tail --quiet --bytes=1 ${to})" ] && printf '\n' >> ${to};
+    fi
     /bin/cat ${from} >> ${to};
     echo " updated from version ${TO_VERSION} (${TO_DATE}) to version ${FROM_VERSION} (${FROM_DATE})."
-  else
-    echo " version ${TO_VERSION} (${TO_DATE}) already exists. Did not update."
   fi
 }
 
 function main() {
-  # echo "Check xtrace."
+  debug "Check xtrace."
   if [ "${XTRACE}" == "y" ]; then set -o xtrace; fi
 
-  # echo "Check script version and date. Make update happen only when newest version is not yet present."
-  SCRIPT_VERSION=$(grep --no-messages "^# Script version *: " ${0} | awk -F: '{print $2}' | awk '{print $1}' | sed -e 's/^[[:space:]]*//' | bc --mathlib)
-  SCRIPT_DATE=$(grep --no-messages "^# Script date *: " ${0} | awk -F: '{print $2}' | awk '{print $1}' | sed -e 's/^[[:space:]]*//')
-  # echo "${0} version: ${SCRIPT_VERSION} (${SCRIPT_DATE})"
+  debug "Check script version and date."
+  SCRIPT_VERSION=$(/bin/grep --no-messages "^# Script version *: " ${0} | /usr/bin/tail --lines 1 | /usr/bin/awk --field-separator=':' '{print $2}' | /usr/bin/awk '{print $1}' | /bin/sed --expression='s/^[[:space:]]*//' | /usr/bin/bc --mathlib)
+  SCRIPT_DATE=$(/bin/grep --no-messages "^# Script date *: " ${0} | /usr/bin/tail --lines 1 | /usr/bin/awk --field-separator=':' '{print $2}' | /usr/bin/awk '{print $1}' | /bin/sed --expression='s/^[[:space:]]*//')
+  debug "${0} version: ${SCRIPT_VERSION} (${SCRIPT_DATE})"
 
-  update ${LOCALREPO}/.my_profile ${HOME}/.profile
-  update ${LOCALREPO}/.my_bashrc ${HOME}/.bashrc
-  update ${LOCALREPO}/.my_bash_aliases ${HOME}/.bash_aliases
-  update ${LOCALREPO}/.my_vimrc ${HOME}/.vimrc
-  update ${LOCALREPO}/.my_screenrc ${HOME}/.screenrc
+  if [ "${TEST}" == "y" ]; then
+    update ${LOCALREPO}/.my_test ${HOME}/.test
+  else
+    update ${LOCALREPO}/.my_profile ${HOME}/.profile; # source ${HOME}/.profile
+    update ${LOCALREPO}/.my_bashrc ${HOME}/.bashrc; source ${HOME}/.bashrc
+    update ${LOCALREPO}/.my_bash_aliases ${HOME}/.bash_aliases; source ${HOME}/.bash_aliases
+    update ${LOCALREPO}/.my_vimrc ${HOME}/.vimrc
+    update ${LOCALREPO}/.my_screenrc ${HOME}/.screenrc
 
-  # update ${HOME}/.config/htop/htoprc
-  # Beware! This file is rewritten by htop when settings are changed in the interface.
-  # The parser is also very primitive, and not human-friendly.
-  # color_scheme=
-  # Default = 0
-  # Monochromatic = 1
-  # Black on White = 2
-  # Light Terminal = 3
-  # MC = 4
-  # Black Night = 5
-  # Broken Gray = 6
-  # I like Broken Gray, ie color_scheme=6
-  sed --in-place=.bak --expression 's/^color_scheme=.*$/color_scheme=6/' ${HOME}/.config/htop/htoprc
-  echo "${HOME}/.config/htop/htoprc color scheme updated."
+    # update ${LOCALREPO}/.my_htoprc ${HOME}/.config/htop/htoprc
+    # Beware! This file is rewritten by htop when settings are changed in the interface.
+    # The parser is also very primitive, and not human-friendly.
+    # color_scheme=
+    # Default = 0
+    # Monochromatic = 1
+    # Black on White = 2
+    # Light Terminal = 3
+    # MC = 4
+    # Black Night = 5
+    # Broken Gray = 6
+    # I like Broken Gray, ie color_scheme=6
+    printf "==> ${HOME}/.config/htop/htoprc"
+    /bin/sed --in-place=.bak --expression='s/^color_scheme=.*$/color_scheme=6/' ${HOME}/.config/htop/htoprc
+    echo " color scheme updated."
+  fi
 
   exit $?
 }
@@ -126,6 +149,8 @@ LOCALREPO="${HOME}/${REPO}"
 while [ $# -gt 0 ]; do
   case $1 in
   (-h|--help) usage 2>&1;;
+  (-d|--debug) DEBUG="y"; shift;;
+  (-t|--test) TEST="y"; shift;;
   (-v|--version) version 2>&1;;
   (-x|--xtrace) XTRACE="y"; set -o xtrace; shift;;
   (--) shift; break;;
